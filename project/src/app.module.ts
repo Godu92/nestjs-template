78bf10119b819073aebba73637a37d014398b04e
douglas.shoemaker <douglas.shoemaker@mantech.com>
2024-11-21 15:00:31 -0500

    TypeORM and PG added

--- project/src/app.module.ts
+++ project/src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'db',
+      port: 5432,
+      username: 'username',
+      password: 'password123!',
+      database: 'db',
+      entities: [],
+      synchronize: true,
+      autoLoadEntities: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
